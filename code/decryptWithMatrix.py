eb1ffe6b677ac56e829d6bba84f2fd797b011500
Sophia Dasser <sophiadasser24@gmail.com>
2024-06-09 23:13:17 -0400

    encrypt working, with english key

--- code/decryptWithMatrix.py
+++ code/decryptWithMatrix.py
@@ -1,9 +1,9 @@
 import sys
 import numpy as np
 from encryptWithMatrix import *
-from numpy.linalg import inv
 
 def modInv(a, m):
+    a = a % m
     for x in range(1, m):
         if (a * x) % m == 1:
             return x
@@ -11,43 +11,40 @@ def modInv(a, m):
 
 def matrixModInv(matrix, modulus):
     det = int(np.round(np.linalg.det(matrix)))
-    detInv = modInv(det % modulus, modulus)
-    
+    det = det % modulus
+    try:
+        detInv = modInv(det, modulus)
+    except ValueError as e:
+        raise ValueError(f"Matrix determinant {det} has no modular inverse under modulo {modulus}.") from e
     adjugate = np.array([[matrix[1,1], -matrix[0,1]], 
                          [-matrix[1,0], matrix[0,0]]])
-    
     matrixInv = (detInv * adjugate) % modulus
-    
     return matrixInv
 
 def decryptMessage(message, encryptionMatrix):
     messageNumbers = textToNumbers(message)
     decryptedNumbers = []
-
     decryptionMatrix = matrixModInv(encryptionMatrix, 26)
-    
     for i in range(0, len(messageNumbers), 2):
         pair = np.array(messageNumbers[i:i+2])
         decryptedPair = np.dot(decryptionMatrix, pair) % 26
         decryptedPair = decryptedPair.astype(int)
         decryptedNumbers.extend(decryptedPair)
-
-    # Remove the last number if it corresponds to 'q'
     if decryptedNumbers[-1] == (ord('q') - ord('a')):
         decryptedNumbers.pop()
-    
     return numbersToText(decryptedNumbers)
 
+def parseArgs(args):
+    if len(args) != 3:
+        print("Incorrect number of arguments. Usage: make decrypt ARGS=\"ciphertextMessage 4letterKey\"")
+        sys.exit(1)
+    message = args[1]
+    key = args[2]
+    return message, key
+
 if __name__ == "__main__":
-    message, matrix = parseArgs(sys.argv)
+    message, key = parseArgs(sys.argv)
     preparedMessage = prepareMessage(message)
+    matrix = createMatrixFromKey(key)
     decryptedMessage = decryptMessage(preparedMessage, matrix)
     print(decryptedMessage)
-
-
-
-
-
-
-
-
--- code/encryptWithMatrix.py
+++ code/encryptWithMatrix.py
@@ -1,113 +1,41 @@
 import sys
 import numpy as np
 
+def createMatrixFromKey(key):
+    keyMatrix = [[ord(key[0]) - ord('a'), ord(key[1]) - ord('a')],
+                 [ord(key[2]) - ord('a'), ord(key[3]) - ord('a')]]
+    return np.array(keyMatrix)
 
 
-def textToNumbers(text):
+def encryptMessage(plainText, key):
+    keyMatrix = createMatrixFromKey(key)
+    print(keyMatrix)
+    textVector = [ord(char) - ord('a') for char in plainText]
+    if len(textVector) % 2 != 0:
+        textVector.append(ord('x') - ord('a'))  # Padding if plaintext length is odd
 
-	numberArray = []
+    encryptedText = ''
+    for i in range(0, len(textVector), 2):
+        vector = np.array(textVector[i:i+2]).reshape(2, 1)
+        encryptedVector = np.dot(keyMatrix, vector) % 26
+        encryptedText += chr(encryptedVector[0, 0] + ord('a'))
+        encryptedText += chr(encryptedVector[1, 0] + ord('a'))
 
-	for char in text:
-		numberOfChar = ord(char) - ord('a')
-		numberArray.append(numberOfChar)
+    return encryptedText
 
-	#print(numberArray)
-
-	return numberArray
-
-def numbersToText(numbers):
-
-	text = ''
-
-	for number in numbers:
-		charOfNumber = chr( number + ord('a'))
-		text += charOfNumber
-
-
-	return text
-
-def encryptMessage(message, encryptionMatrix):
-
-	#Given plain text message, convert letters to its index in alphabet
-
-	messageInNumbers = textToNumbers(message)
-	encryptedArrayOfVectors = []
-
-	#Split the message into pairs to put in a 2 dimensional vector
-
-	for i in range(0, len(messageInNumbers), 2):
-		
-		pairVector = np.array(messageInNumbers[i:i+2])
-		#print(pairVector)
-
-		'''
-		Next encrupt the pair by applying matrix multiplication 
-		
-		Note: dot product and matrix mult. is the same when 
-		multplying a vector and a matrix
-
-		'''
-
-		encryptedPair = np.dot(encryptionMatrix,pairVector) % 26
-
-		#Add encrypted par to the new encrypted array of vectors
-
-
-		# The use of .extend() instead of .append() is necessary adds 
-		# each element of an iterable (e.g., list) to the end of the list
-		# rather adding its argument as a single element to the end of the list.
-
-		encryptedArrayOfVectors.extend(encryptedPair) 
-
-	return numbersToText(encryptedArrayOfVectors)
-
-
-# Miscellaneous Functions:
-
-
-def parseArgs(args):
-
-	if len(args) != 3:
-		print("Incorrect number of arguments. Usage: make encrypt ARGS=\"plaintextMessage fourCommaSeperatedValuesForEncryptionMatrix\"")
-		sys.exit(1)
-
-	# Parse plaintext message:
-	message = args[1]
-
-	# Parse CSV for matrix
-	matrixValues = list(map(int, args[2].split(',')))
-
-	if len(matrixValues) != 4:
-		print("Encryption matrix must have exactly 4 values.") 
-		sys.exit(1)
-
-	# Convert the list of values into a 2x2 matrix
-
-	matrix = np.array(matrixValues).reshape(2, 2)
-
-	return message, matrix
-
-def prepareMessage(message):
-	
-	# Convert the message to lowercase
-	message = message.lower()
-
-	# If the message length is odd, add a padding character (e.g., 'x')
-	if len(message) % 2 != 0:
-		message += 'q'
-
-	return message
-
-
-# Main Function:
 
 if __name__ == "__main__":
-    message, matrix = parseArgs(sys.argv)
-    preparedMessage = prepareMessage(message)
-    encryptedMessage = encryptMessage(preparedMessage, matrix)
-    print(encryptedMessage)
 
+    if len(sys.argv) != 3:
+        print("Usage: make encrypt ARGS=\"plaintext key\"")
+        sys.exit(1)
 
+    plainText = sys.argv[1]
+    key = sys.argv[2]
 
+    if len(key) != 4:
+        print("Key must be 4 characters long.")
+        sys.exit(1)
 
-
+    cipherText = encryptMessage(plainText, key)
+    print(f"Encrypted Text: {cipherText}")
